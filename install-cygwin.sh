7ad36b53992262668fb9909fbf696b413cf53435
Peter Klein <peter@uytr.org>
2012-11-27 17:40:24 -0500

    Make .zsh default shell and install shell shortcuts

--- install-cygwin.sh
+++ install-cygwin.sh
@@ -13,3 +13,9 @@ for f in $FILES
 do
     ln -fs $(realpath `dirname $0`)/$f ~
 done
+
+# Set up chere
+chere -i -c -f -t mintty -e "Open zsh"
+
+# There should be a better way to do this (set zsh in /etc/passwd)
+sed -i -e 's/\(pklein.*\:).*/\1\/bin\/zsh/g' /etc/passwd
