8f8ddefbef05e483f5c6961855d3dde7e5622b4f
Peter Klein <peter@uytr.org>
2012-11-28 08:35:17 -0500

    Quote windows paths

--- install-windows.bat
+++ install-windows.bat
@@ -1,11 +1,12 @@
 @echo off
-rd /S /Q %USERPROFILE%\vimfiles
-mklink /D %USERPROFILE%\vimfiles %~dp0.vim
-del /F %USERPROFILE%\_vimrc
-mklink %USERPROFILE%\_vimrc %~dp0.vimrc
+rd /S /Q "%USERPROFILE%\vimfiles"
+mklink /D "%USERPROFILE%\vimfiles" "%~dp0.vim"
+del /F "%USERPROFILE%\_vimrc"
+mklink "%USERPROFILE%\_vimrc" "%~dp0.vimrc"
 del /F C:\.emacs
-mklink C:\.emacs %~dp0.emacs
-del /F %USERPROFILE%\.gitconfig
-mklink %USERPROFILE%\.gitconfig %~dp0.gitconfig
+mklink C:\.emacs "%~dp0.emacs"
+del /F "%USERPROFILE%\.gitconfig"
+mklink "%USERPROFILE%\.gitconfig" "%~dp0.gitconfig"
 
-regedit /s %~dp0\putty_default_settings.reg
+regedit /s "%~dp0\putty_default_settings.reg"
+
