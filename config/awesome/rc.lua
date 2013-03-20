e2f585a0ffc8941610d7345697a89a0745ba0d06
Peter Klein <peter@uytr.org>
2013-03-20 00:51:03 -0400

    awesome: Update awesome clock format

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -110,7 +110,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock()
+mytextclock = awful.widget.textclock("%Y-%m-%d %I:%M %p",15)
 
 -- Create a wibox for each screen and add it
 mywibox = {}
