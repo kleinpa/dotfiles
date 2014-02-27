6bfd1db9f56fd39e6209227ce34abfdb01d5595a
Peter Klein <peter@uytr.org>
2014-02-27 16:47:34 -0500

    sublime: Import settings

--- install-windows.bat
+++ install-windows.bat
@@ -1,5 +1,5 @@
 @echo off
-del /F /Q "%USERPROFILE%\AppData\Roaming\.emacs.d"
+RD /S /Q "%USERPROFILE%\AppData\Roaming\.emacs.d"
 mklink /D "%USERPROFILE%\AppData\Roaming\.emacs.d" "%~dp0emacs.d"
 del /F /Q "%USERPROFILE%\.gitignore.global"
 mklink "%USERPROFILE%\.gitignore.global" "%~dp0gitignore.global"
@@ -8,8 +8,12 @@ mklink "%USERPROFILE%\.gitconfig" "%~dp0gitconfig"
 
 del /F /Q "%USERPROFILE%\.vagrant.d\Vagrantfile"
 mklink "%USERPROFILE%\.vagrant.d\Vagrantfile" "%~dp0vagrant.d\Vagrantfile"
-del /F /Q "%USERPROFILE%\.vagrant.d\scripts"
+RD /S /Q "%USERPROFILE%\.vagrant.d\scripts"
 mklink /D "%USERPROFILE%\.vagrant.d\scripts" "%~dp0vagrant.d\scripts"
 
 regedit /s "%~dp0\putty_default_settings.reg"
 regedit /s "%~dp0\diffmerge_settings.reg"
+
+RD /S /Q "%USERPROFILE%\AppData\Roaming\Sublime Text 3\Packages\User"
+mklink /D "%USERPROFILE%\AppData\Roaming\Sublime Text 3\Packages\User" "%~dp0Sublime Text"
+PAUSE
