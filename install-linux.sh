5bdf98e7847b97b989952c017cb31d2c55f394c4
Peter Klein <peter@uytr.org>
2014-04-24 13:10:14 -0400

    Only link config sub-folders
    Along the same lines, only link ssh config, not enture ssh directory. Too scared
    of commiting a private key.

--- install-linux.sh
+++ install-linux.sh
@@ -8,10 +8,11 @@ ln -Tfs $(realpath `dirname $0`)/minttyrc ~/.minttyrc
 ln -Tfs $(realpath `dirname $0`)/emacs.d ~/.emacs.d
 ln -Tfs $(realpath `dirname $0`)/Xresources ~/.Xresources
 ln -Tfs $(realpath `dirname $0`)/xinitrc ~/.xinitrc
-ln -Tfs $(realpath `dirname $0`)/config ~/.config
+ln -Tfs $(realpath `dirname $0`)/awesome ~/.config/awesome
+ln -Tfs "$(realpath `dirname $0`)/Sublime Text" ~/.config/sublime-text-3/Packages
 ln -Tfs $(realpath `dirname $0`)/profile ~/.profile
 ln -Tfs $(realpath `dirname $0`)/zprofile ~/.zprofile
-ln -Tfs $(realpath `dirname $0`)/ssh ~/.ssh
+ln -Tfs $(realpath `dirname $0`)/ssh/config ~/.ssh/config
 ln -Tfs $(realpath `dirname $0`)/bin ~/bin
 
 mkdir -p ~/.vagrant.d/
