960547dddee18e56fa4ea033f6680e6ff3269b62
Peter Klein <peter@uytr.org>
2014-09-09 22:02:08 -0400

    emacs: Change loading filename logic

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,27 +1,42 @@
 (defun rc (fn)
-  (concat (file-name-directory load-file-name) (convert-standard-filename "rc/") fn ".el"))
+  (concat (file-name-directory load-file-name) (convert-standard-filename "rc/") fn))
 
-(load-file (rc "server"))
+(load-file (rc "server.el"))
+(load-file (rc "packages.el"))
 
 ;; Appearance Settings
-(load-file (rc "decor"))
+(load-file (rc "decor.el"))
 
 ;; Language Modes
-(load-file (rc "c"))
-(load-file (rc "java"))
-(load-file (rc "javascript"))
-(load-file (rc "latex"))
-(load-file (rc "lua"))
-(load-file (rc "markdown"))
-(load-file (rc "scheme"))
+(load-file (rc "c.el"))
+(load-file (rc "css.el"))
+(load-file (rc "java.el"))
+(load-file (rc "javascript.el"))
+(load-file (rc "latex.el"))
+(load-file (rc "lua.el"))
+(load-file (rc "markdown.el"))
+(load-file (rc "scheme.el"))
 
 ;; Other
-(load-file (rc "common-hooks"))
-(load-file (rc "woman"))
+(load-file (rc "common-hooks.el"))
+(load-file (rc "woman.el"))
 
 ;; Platform Specific Settings
-(let ((platform-rc (rc (concat "platform-" (symbol-name system-type)))))
+(let ((platform-rc (rc (concat "platform-" (symbol-name system-type) ".el"))))
   (when (file-readable-p platform-rc)
     (load-file platform-rc)))
 
-(load-file (rc "old"))
+(load-file (rc "old.el"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
