42c97b5d7dc29c231d9ba412ad980ec2becf33cf
Peter Klein <peter@uytr.org>
2015-04-14 23:49:45 +0000

    install: Make script more robust

--- install-linux.sh
+++ install-linux.sh
@@ -1,3 +1,8 @@
+command -v realpath >/dev/null 2>&1 || {
+  echo "This script requires the program 'realpath', aborting.";
+  exit 1;
+}
+
 ln -Tfs $(realpath `dirname $0`)/zshrc ~/.zshrc
 ln -Tfs $(realpath `dirname $0`)/zsh ~/.zsh
 ln -Tfs $(realpath `dirname $0`)/vimrc ~/.vimrc
@@ -8,10 +13,11 @@ ln -Tfs $(realpath `dirname $0`)/Xresources ~/.Xresources
 ln -Tfs $(realpath `dirname $0`)/xinitrc ~/.xinitrc
 ln -Tfs $(realpath `dirname $0`)/profile ~/.profile
 ln -Tfs $(realpath `dirname $0`)/zprofile ~/.zprofile
-ln -Tfs $(realpath `dirname $0`)/ssh/config ~/.ssh/config
-ln -Tfs $(realpath `dirname $0`)/bin ~/bin
 
-mkdir ~/.emacs.d
+mkdir -p ~/.ssh
+ln -Tfs $(realpath `dirname $0`)/ssh/config ~/.ssh/config
+
+mkdir -p ~/.emacs.d
 ln -Tfs $(realpath `dirname $0`)/emacs.d/init.el ~/.emacs.d/init.el
 
 mkdir -p ~/.vagrant.d/
@@ -19,5 +25,9 @@ ln -Tfs $(realpath `dirname $0`)/vagrant.d/Vagrantfile ~/.vagrant.d/Vagrantfile
 ln -Tfs $(realpath `dirname $0`)/vagrant.d/scripts ~/.vagrant.d/scripts
 
 mkdir -p ~/.config
-ln -Tfs "$(realpath `dirname $0`)/Sublime Text" ~/.config/sublime-text-3/Packages
 ln -Tfs $(realpath `dirname $0`)/awesome ~/.config/awesome
+
+mkdir -p ~/.config/sublime-text-3
+ln -Tfs "$(realpath `dirname $0`)/Sublime Text" ~/.config/sublime-text-3/Packages
+
+touch ~/.hushlogin
