3f86e01944b4c6ccde0a73adc0c123b30e12e363
Peter Klein <kleinpa00@gmail.com>
2020-01-21 12:27:32 -0500

    installer: Add source file checking and log errors to stderr

--- install-linux.sh
+++ install-linux.sh
@@ -1,42 +1,56 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash -ue
+
+# These files are directly linked in from the repository. The link source is the
+# file but without the first dot.
+
+BASIC_LINK=(
+    .bash_profile
+    .bashrc
+    .bashrc.d/aliases
+    .bashrc.d/common
+    .bashrc.d/history
+    .bashrc.d/prompt
+    .bin/hash_color
+    .emacs.d/init.el
+    .emacs.d/lisp
+    .gitconfig
+    .gitignore.global
+    .inputrc
+    .minttyrc
+    .profile
+    .ssh/config
+    .tmux.conf
+    .vimrc
+    .xinitrc
+    .Xresources
+    .Xresources
+    .zprofile
+    .zsh
+    .zshrc
+)
 
 command -v realpath >/dev/null 2>&1 || {
-  echo "This script requires the program 'realpath', aborting.";
+  echo "This script requires the program 'realpath', aborting." 1>&2;
   exit 1;
 }
 
-BASIC_LINK=(
-    bash_profile
-    bashrc
-    bashrc.d/aliases
-    bashrc.d/common
-    bashrc.d/history
-    bashrc.d/prompt
-    bin
-    emacs.d/init.el
-    emacs.d/lisp
-    gitconfig
-    gitignore.global
-    inputrc
-    minttyrc
-    profile
-    ssh/config
-    tmux.conf
-    vimrc
-    xinitrc
-    Xresources
-    Xresources
-    zprofile
-    zsh
-    zshrc
-)
+SOURCE="$(realpath $(dirname $0))"
 
+# Check that all link source files exist
 for f in ${BASIC_LINK[@]}; do
-    [ -d "$(dirname ~/.${f})" ] || mkdir -p "$(dirname ~/.${f})"
-    ln -Tfs "$(realpath `dirname $0`)/${f}" "$HOME/.${f}"
-    echo "Linked .${f}"
+  if [[ ! -r "${SOURCE}/${f##.}" ]]; then
+    echo "Source file for '${f}' does not exist in this directory" 1>&2
+    exit 1
+  fi
 done
 
-touch ~/.hushlogin
-mkdir -p ~/.config
+for f in ${BASIC_LINK[@]}; do
+  echo "Linking ${f}" 1>&2
+  [[ -d "$(dirname ~/${f})" ]] || mkdir -p "$(dirname ~/${f})"
+  ln -Tfs "${SOURCE}/${f##.}" "$HOME/${f}"
+done
+
+# Miscellaneous
+
+echo "Creating empty ~/.hushlogin" 1>&2
+touch "${HOME}/.hushlogin"
