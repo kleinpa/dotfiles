d8949cc5ecd2578ef8f0eca218783cd17a932a70
Peter Klein <kleinpa00@gmail.com>
2025-05-14 17:38:42 -0400

    Enable Powershell profile

--- install-windows.ps1
+++ install-windows.ps1
@@ -11,6 +11,7 @@ function Make-Directory($Path) {
 
 function Delete($Path) {
     if (Test-Path $Path) {
+        Write-Warning "Deleting $Path"
         Remove-Item -Recurse $Path
     }
 }
@@ -20,14 +21,6 @@ Function New-SymLink ($Link, $Target) {
 }
 
 $UserProfile = [environment]::getFolderPath("UserProfile")
-$ApplicationData = [environment]::getFolderPath("ApplicationData")
-
-# if("Emacs" -in $Configs -Or "All" -in $Configs) {
-#     Make-Directory "${ApplicationData}\.emacs.d"
-#     Delete "${ApplicationData}\.emacs.d\init.el"
-#     New-SymLink "${ApplicationData}\.emacs.d\init.el" "${PSScriptRoot}\emacs.d\init.el"
-#     New-SymLink "${ApplicationData}\.emacs.d\lisp" "${PSScriptRoot}\emacs.d\lisp"
-# }
 
 if("Git" -in $Configs -Or "All" -in $Configs) {
     Delete "${UserProfile}\.gitignore.global"
@@ -43,21 +36,9 @@ if("ssh" -in $Configs -Or "All" -in $Configs) {
 }
 
 if("Powershell" -in $Configs -Or "All" -in $Configs) {
-    # Make-Directory "${UserProfile}\Documents\WindowsPowerShell\"
-    # Delete "${UserProfile}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-    # New-SymLink "${UserProfile}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "${PSScriptRoot}\Powershell\profile.ps1"
-    # rd /s /q "${UserProfile}\Documents\WindowsPowerShell\Modules"
-    # New-SymLink /d "${UserProfile}\Documents\WindowsPowerShell\Modules" "${PSScriptRoot}\Powershell\Modules"
-    # "${PSScriptRoot}\Powershell\setup.bat"
+    Make-Directory "${UserProfile}\Documents\WindowsPowerShell\"
+    Delete "${UserProfile}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+    New-SymLink "${UserProfile}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "${PSScriptRoot}\Powershell\profile.ps1"
+    "${PSScriptRoot}\Powershell\setup.bat"
 }
 
-# if("WSL" -in $Configs -Or "All" -in $Configs) {
-#     if (Get-Command "bash.exe" -ErrorAction SilentlyContinue)
-#     {
-#         $WSLHome = $(Invoke-Command {cd ~; bash -c pwd})
-#         Push-Location "${PSScriptRoot}"
-#         bash.exe -c "ln -s ${WSLHome}/Projects ~/projects"
-#         bash.exe -c "~/projects/dotfiles/install-linux.sh"
-#         Pop-Location
-#     }
-# }
